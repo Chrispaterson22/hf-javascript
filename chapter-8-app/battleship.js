87f4b63a04987fef2212422a9d44fb38ebf78d6a
Chris Paterson <chrispaterson22@gmail.com>
2020-11-13 17:04:50 +0000

    WIP chapter 8 battleship.js

--- chapter-8-app/battleship.js
+++ chapter-8-app/battleship.js
@@ -29,13 +29,73 @@ var model = {
       var index = ship.locations.indexOf(guess);
       if (index >= 0) {
         ship.hits[index] = "hit";
+        view.displayHit(guess);
+        view.displayMessage("HIT!");
+        if (this.isSunk(ship)) {
+          view.displayMessage("You sank my battleship!");
+          this.shipsSunk++;
+        }
         return true;
       }
     }
+    view.displayMiss(guess);
+    view.displayMessage("You missed.");
     return false;
+  },
+
+  isSunk: function (ship) {
+    for (var i = 0; i < this.shipLength; i++) {
+      if (ship.hits[i] !== "hit") {
+        return false;
+      }
+    }
+    return true;
   }
 };
 
+var controller = {
+  guesses: 0,
+
+  processGuess: function(guess) {
+    var location = parseGuess(guess);
+    if (location) {
+      this.guesses++;
+      var hit = model.fire(location);
+      if (hit && model.shipsSunk === model.numShips) {
+        view.displayMessage("You sank all my battleships, in " +
+                              this.guesses + " guesses");
+      }
+    }
+  },
+};
+
+function parseGuess(guess) {
+  var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+
+  if (guess === null || guess.length !== 2) {
+    alert("Oops, please enter a letter and a number on the board.")
+  }
+  else {
+    var firstChar = guess.charAt(0);
+    var row = alphabet.indexOf(firstChar);
+    var column = guess.charAt(1);
+
+    if (isNaN(row) || isNaN(column)) {
+      alert("Oops, that isn't on the board.");
+    }
+    else if (row < 0 || row >= model.boardSize || column < 0 ||
+              column >= model.boardSize) {
+      alert("Oops, that's off the board!");
+    }
+    else {
+      return row + column; // concatenating num + string = string
+    }
+  }
+  return null; // if get here there was a failed check, so return null
+}
+
+// test code for view object
+/*
 view.displayMiss("00");
 view.displayHit("34");
 view.displayMiss("55");
@@ -44,3 +104,49 @@ view.displayMiss("25");
 view.displayHit("26");
 
 view.displayMessage("Tap tap, is this thing on?");
+*/
+
+// test code for model object
+/*
+model.fire("53"); // miss
+
+model.fire("06"); // hit
+model.fire("16"); // hit
+model.fire("26"); // hit
+
+model.fire("34"); // hit
+model.fire("24"); // hit
+model.fire("44"); // hit
+
+model.fire("12"); // hit
+model.fire("11"); // hit
+model.fire("10"); // hit
+*/
+
+// test code for parseGuess function
+/*
+console.log(parseGuess("A0"));
+console.log(parseGuess("B6"));
+console.log(parseGuess("G3"));
+console.log(parseGuess("H0"));
+console.log(parseGuess("A7"));
+*/
+
+// controller test code
+// You should see three ships on the board, one miss, and the message
+// "You sank all my battleships in 10 guesses"
+/*
+controller.processGuess("A0"); // miss
+
+controller.processGuess("A6"); // hit
+controller.processGuess("B6"); // hit
+controller.processGuess("C6"); // hit
+
+controller.processGuess("C4"); // hit
+controller.processGuess("D4"); // hit
+controller.processGuess("E4"); // hit
+
+controller.processGuess("B0"); // hit
+controller.processGuess("B1"); // hit
+controller.processGuess("B2"); // hit
+*/
